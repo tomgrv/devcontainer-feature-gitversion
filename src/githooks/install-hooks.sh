1770c291a376b157aa846b4e3d990d98ec9f150f
Thomas G. <tomgrv@users.perspikapps.fr>
2024-06-22 21:18:48 +0000

    fix: update githooks and gitutils

--- src/githooks/install-hooks.sh
+++ src/githooks/install-hooks.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 set -e
 
-### For each sh hook starting with _ in the feature directory, copy it to the hooks directory
-echo "Copying hooks to $target..."
-find $source -type f -name "_*.sh" | while read hook; do
-    hookName=$(basename $hook | sed 's/^_//;s/\.sh$//')
-    cp $hook $target/$hookName
-    chmod +x $target/$hookName
-    echo "Copied hook $hook => $target/$hookName"
+### For each script starting with _ and ending with .sh, Create a symbolic link to the hook script without _ and sh extension
+echo "Configuring hooks..."
+for script in $target/_*.sh; do
+    hook=$(basename $script | sed -e 's/^_//g' -e 's/.sh$//g')
+    ln -sf $script $target/$hook
+    echo "Created hook $hook => $script"
 done
